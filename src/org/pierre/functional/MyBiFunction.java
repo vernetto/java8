3992034a5a2b3d718bb4129bf992109992a3e869
vernetto <publicpierre@gmail.com>
2019-12-29 10:18:44 +0100

    second commit

--- src/org/pierre/functional/MyBiFunction.java
+++ src/org/pierre/functional/MyBiFunction.java
@@ -5,8 +5,7 @@ import java.util.List;
 import java.util.function.BiFunction;
 
 public class MyBiFunction {
-    private static <T, U, R> List<R> listCombiner(
-            List<T> list1, List<U> list2, BiFunction<T, U, R> combiner) {
+    private static <T, U, R> List<R> listCombiner(List<T> list1, List<U> list2, BiFunction<T, U, R> combiner) {
         List<R> result = new ArrayList<>();
         for (int i = 0; i < list1.size(); i++) {
             result.add(combiner.apply(list1.get(i), list2.get(i)));
